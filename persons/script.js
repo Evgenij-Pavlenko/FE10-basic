4b8f4ebddf193b8cfd90c29ce5c3976982d1b568
Ievgenij_Pavlenko <evgenij.pavlenko@gmail.com>
2020-05-22 00:58:54 +0200

    add age Sorted up and down

--- persons/script.js
+++ persons/script.js
@@ -1,15 +1,12 @@
 function showUsersTable(persons = []) {
-    const sortedPersons = persons.sort(function (a, b) {
-        return a.age - b.age;
-    });
 
     let averageAge = 0;
     let minAge = persons[0].age;
     let maxAge = persons[0].age;
     let innerHtmlTableBody = '';
     for (let i = 0; i < persons.length; i++) {
-        innerHtmlTableBody += '<tr><td>' + sortedPersons[i].name + '</td><td>' + sortedPersons[i].age + '</td><td>' + sortedPersons[i].city + '</td></tr>';
-        averageAge += + sortedPersons[i].age;
+        innerHtmlTableBody += '<tr><td>' + persons[i].name + '</td><td>' + persons[i].age + '</td><td>' + persons[i].city + '</td></tr>';
+        averageAge += + persons[i].age;
         if (persons[i].age > maxAge)
             maxAge = persons[i].age;
         if (persons[i].age < minAge)
@@ -22,18 +19,43 @@ function showUsersTable(persons = []) {
 }
 
 let persons = [];
+let ageSort = true;
 
 const form$ = document.getElementsByTagName('form')[0];
-form$.addEventListener('submit', function(event) {
+form$.addEventListener('submit', function (event) {
     event.preventDefault();
 
-// Должны ли input в форме очищаться после нажатия на Submit?
     let person = {};
     person.name = document.querySelector('input[name=name]').value;
     person.age = document.querySelector('input[name=age]').value;
     person.city = document.querySelector('input[name=city]').value;
 
     persons.push(person);
+    // Должны ли input в форме очищаться после нажатия на Submit?
+    document.querySelector('input[name=name]').value = null;
+    document.querySelector('input[name=age]').value = null;
+    document.querySelector('input[name=city]').value = null;
 
     showUsersTable(persons);
+})
+// -------------------Сортировка по возрасту туда-сюда----------------------------
+// const th$ = document.getElementsByName('th[name=age]');  //** не работает. Как-то не так передаю параметр элемента по NAME? */
+const th$ = document.getElementById('age');     //работает
+// age.addEventListener('click', function (event) {   //тоже работает
+th$.addEventListener('click', function (event) {
+    event.preventDefault();
+
+    let sortedPersons;
+
+    if (ageSort) {
+        sortedPersons = persons.sort(function (a, b) {
+            return a.age - b.age;
+        });
+    } else {
+        sortedPersons = persons.sort(function (a, b) {
+            return b.age - a.age;
+        });
+    }
+    ageSort = !ageSort;
+    showUsersTable(sortedPersons);
 })
