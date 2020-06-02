6ce23ff94637d12b5b9a3bf56a62be7ea86b5a47
Ievgenij_Pavlenko <evgenij.pavlenko@gmail.com>
2020-06-02 20:39:29 +0200

    add direct sorting by Name, Email, Feedback, Date

--- Test_2020-05-26_feedback_form/script.js
+++ Test_2020-05-26_feedback_form/script.js
@@ -10,7 +10,7 @@ function showFeedbacksTable(feedbacks = []) {
     document.getElementsByTagName('tbody')[0].innerHTML = innerHtmlTableBody;
     // document.getElementById('count').style.display = "block";
     let $tfoot = document.querySelector('tfoot div.alert');
-    $tfoot.style.display="block";
+    $tfoot.style.display = "block";
     $tfoot.innerHTML = 'Total number of records: <b>' + feedbacks.length + '</b>';
 
 
@@ -39,3 +39,97 @@ form$.addEventListener('submit', function (event) {
 
     showFeedbacksTable(feedbacks);
 })
+
+let sortName = true;
+let sortEmail = true;
+let sortDate = true;
+let sortFb = true;
+let sortedFeedback;
+// ----------------------Sort by name----------------------
+const $name = document.getElementById('name');
+$name.addEventListener('click', function (event) {
+    event.preventDefault();
+    if (sortName) {
+        console.log("sortName= true " + sortName);
+        sortedFeedback = feedbacks.sort(function (a, b) {
+            if (a.name > b.name) return 1;
+            if (a.name < b.name) return -1;
+            return 0;
+        });
+    } else {
+        console.log("sortName= false " + sortName);
+        sortedFeedback = feedbacks.sort(function (a, b) {
+            if (a.name < b.name) return 1;
+            if (a.name > b.name) return -1;
+            return 0;
+        });
+    }
+    sortName = !sortName;
+    showFeedbacksTable(feedbacks);
+});
+// ----------------------Sort by Email----------------------
+const $email = document.getElementById('email');
+$email.addEventListener('click', function (event) {
+    event.preventDefault();
+    if (sortEmail) {
+        console.log("sortemail= true " + sortEmail);
+        sortedFeedback = feedbacks.sort(function (a, b) {
+            if (a.email > b.email) return 1;
+            if (a.email < b.email) return -1;
+            return 0;
+        });
+    } else {
+        console.log("sortEmail= false " + sortEmail);
+        sortedFeedback = feedbacks.sort(function (a, b) {
+            if (a.email < b.email) return 1;
+            if (a.email > b.email) return -1;
+            return 0;
+        });
+    }
+    sortEmail = !sortEmail;
+    showFeedbacksTable(feedbacks);
+});
+// ----------------------Sort by Feedback----------------------
+const $feedback = document.getElementById('feedback');
+$feedback.addEventListener('click', function (event) {
+    event.preventDefault();
+    if (sortFb) {
+        console.log("sortFeedback= true " + sortFb);
+        sortedFeedback = feedbacks.sort(function (a, b) {
+            if (a.feedback > b.feedback) return 1;
+            if (a.feedback < b.feedback) return -1;
+            return 0;
+        });
+    } else {
+        console.log("sortFeedback= false " + sortFb);
+        sortedFeedback = feedbacks.sort(function (a, b) {
+            if (a.feedback < b.feedback) return 1;
+            if (a.feedback > b.feedback) return -1;
+            return 0;
+        });
+    }
+    sortFb = !sortFb;
+    showFeedbacksTable(feedbacks);
+});
+// ----------------------Sort by Date----------------------
+const $date = document.getElementById('date');
+$date.addEventListener('click', function (event) {
+    event.preventDefault();
+    if (sortDate) {
+        console.log("sortDate= true " + sortDate);
+        sortedFeedback = feedbacks.sort(function (a, b) {
+            if (a.currentDate > b.currentDate) return 1;
+            if (a.currentDate < b.currentDate) return -1;
+            return 0;
+        });
+    } else {
+        console.log("sortDate= false " + sortDate);
+        sortedFeedback = feedbacks.sort(function (a, b) {
+            if (a.currentDate < b.currentDate) return 1;
+            if (a.currentDate > b.currentDate) return -1;
+            return 0;
+        });
+    }
+    sortDate = !sortDate;
+    showFeedbacksTable(feedbacks);
+});
