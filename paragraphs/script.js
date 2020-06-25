04f1f3ab7eaf1c9ee5e226159da87acb67e9b1a1
Ievgenij_Pavlenko <evgenij.pavlenko@gmail.com>
2020-06-25 21:43:17 +0200

    Change colors using closures

--- paragraphs/script.js
+++ paragraphs/script.js
@@ -1,11 +1,19 @@
 const colors = ['white', 'red', 'green', 'blue'];
-//                 0       1       2        3  
+//                 0       1       2        3
 
-let currentColor = 0;
-document.querySelectorAll('p').forEach(function(element) {
-    element.addEventListener('click', function() {
-        currentColor++;
-        this.style['background-color'] = colors[currentColor % colors.length];
-    });
+document.querySelectorAll('p').forEach(function (element) {
+    element.addEventListener('click', changeColor(element))
 })
+
+function changeColor(param) {
+    let currentColor = 0;
+    return () => {
+        currentColor++;
+        return param.style['background-color'] = colors[currentColor % colors.length];
+
+        // this.style['background-color'] = color[++currentColor % color.length]; - или так - в одну строчку
+        // this.style.backgroundColor = 'red'; // auch richtig)
+    }
+
+}
 
