6ccc06fdb6d94b421c7fd31edb87b2055574c977
Ievgenij_Pavlenko <evgenij.pavlenko@gmail.com>
2020-06-29 00:25:28 +0200

    correct

--- paragraphs/randomNum.js
+++ paragraphs/randomNum.js
@@ -5,30 +5,34 @@ integer number (from 0 to 100). Number printed must be unique for the certain bu
 Print number not to the console, but below the button.
 */
 
-let randomNum = document.querySelectorAll('button');
-let set = new Set;
+// let randomNum = document.querySelectorAll('button');
+let set = new Set; // если переносить в функцию - постоянно обнуляется. Если оставить здесь - общий сет для всх кнопок()
 function random() {
     let randomNumber = 0;
-    let bool = false;
-    while(!bool){
-        randomNumber = Math.floor(Math.random() * 101);
-        if(!set.has(randomNumber)){
-            set.add(randomNumber)
-            bool = true;
+    let isHas = false;
+    while (!isHas) {
+        if (set.size == 101) {
+            console.log("Set randomNumber is full. Reset set")
+            set.clear();
+        } else {
+            console.log(set);
+            randomNumber = Math.floor(Math.random() * 101);
+            if (!set.has(randomNumber)) {
+                set.add(randomNumber)
+                isHas = true;
+            }
         }
-    }
-    if(set.length ==101){
-        console.log("Set randomNumber is full. Reset set")
-        set.clear();
     }
     return randomNumber;
 }
 
 let buttons = document.getElementsByTagName('button');
 for (let i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener('click', function() {
-        return function() {
-        console.log("For button: " + this.name + " Num is: " + random());
+    buttons[i].addEventListener('click', function () {
+        return function () {
+
+
+            console.log("For button: " + this.name + " Num is: " + random());
         }
-   }());
+    }());
 }
