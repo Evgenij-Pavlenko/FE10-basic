6885b0b41275c5f4aa85aa57444851acd2f742d6
Ievgenij_Pavlenko <evgenij.pavlenko@gmail.com>
2020-06-30 20:17:53 +0200

    correct randomButton with function

--- randomButton/script.js
+++ randomButton/script.js
@@ -1,18 +1,18 @@
 /*
-You have several button in your html code. When you click button, 
-print to console random positive integer number (from 0 to 100). 
+You have several button in your html code. When you click button,
+print to console random positive integer number (from 0 to 100).
 Number printed must be unique for the certain button (can't be repeated).
 
 Print number not to the console, but below the button.
 */
 
-document.querySelectorAll('button').forEach(button => {
+document.querySelectorAll('button').forEach(function(button) {
     button.addEventListener('click', function() {
         console.log(getRandomNonRepetitive());
     })
 });
 
-function getRandomNonRepetitive() {
+function getRandomNonRepetitive(i) {
     let numbers = {};
     return function() {
         if (Object.keys(numbers).length === 9)
