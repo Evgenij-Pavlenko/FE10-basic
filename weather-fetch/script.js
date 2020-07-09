06dca14d3724b2eb14205ec9c0035fadbcd82650
Ievgenij_Pavlenko <evgenij.pavlenko@gmail.com>
2020-07-09 02:16:37 +0200

    add config json with fetch

--- weather-fetch/script.js
+++ weather-fetch/script.js
@@ -1,17 +1,30 @@
-const URL = 'http://api.openweathermap.org/data/2.5/weather';
-const APPID = 'bed156d40174100c73406417523ddaf1';
+// const URL = 'http://api.openweathermap.org/data/2.5/weather';
+// const APPID = 'bed156d40174100c73406417523ddaf1';
 
-const weatherFieldConig = [
-    {name: 'Temperature', field: 'main.temp'},
-    {name: 'Wind deg', field: 'wind.deg'},
-    {name: 'Wind speed', field: 'wind.speed'}
-];
+// const weatherFieldConig = [
+//     {name: 'Temperature', field: 'main.temp'},
+//     {name: 'Wind deg', field: 'wind.deg'},
+//     {name: 'Wind speed', field: 'wind.speed'}
+// ];
+const configJson = "./config.json";
+// const myJson; // Missing initializer in const declaration
+let myJson = {};
+fetch(configJson)
+    .then(response => response.text())
+    .then(text => jsonAdapter(text)); //тут не попадает в myJson
 
-document.querySelector('form').addEventListener('submit', function(event) {
+
+    function jsonAdapter(text){
+        myJson = JSON.parse(text);
+        // console.log(myJson); //тут выводит в консоль
+    };
+    console.log(myJson); //выводит "{}"
+
+document.querySelector('form').addEventListener('submit', function (event) {
     event.preventDefault();
     const country = event.target['country'].value;
     const city = event.target['city'].value;
-  
+
     fetch(`${URL}?country=${country}&q=${city}&APPID=${APPID}`)
         .then(response => response.json())
         .then(json => showWeather(json, event.target));
@@ -21,13 +34,13 @@ function showWeather(json, target) {
     console.log(json);
     const titleElement$ = target.nextElementSibling.querySelector('h5.card-title');
     titleElement$.innerText = `Weather for ${json.name}`;
-    
+
     let html = '';
     weatherFieldConig.forEach((el => {
         html += '<div class="row">';
         html += `<div class="col-6">${el.name}</div><div class="col-6">${eval('json.' + el.field)}</div>`
         html += '</div>';
-    }))    
+    }))
 
     titleElement$.nextElementSibling.innerHTML = html;
 }
