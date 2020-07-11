e6d45dbfbdf1c63214cd23ace1152f29d20a044e
Ievgenij_Pavlenko <evgenij.pavlenko@gmail.com>
2020-07-11 19:34:08 +0200

    Show the measurement units after the value

--- weather-fetch/script.js
+++ weather-fetch/script.js
@@ -7,22 +7,26 @@ fetch('/weather-fetch/config.json')
             event.preventDefault();
             const country = event.target['country'].value;
             const city = event.target['city'].value;
-            // const units = document.querySelector('input[name="unitsRadios"]:checked').value;
+            const units1 = document.querySelector('input[name="unitsRadios"]:checked').value;
 
             // через  target - не получилось((
             // неправильно выборку элемента делаю??
             // const units = event.target['input[name="unitsRadios"]:checked'].value;
             // const units = event.target['unitsRadios:checked'].value;
 
-            fetch(`${config.URL}?country=${country}&q=${city}&APPID=${config.APPID}&units=${units}`)
+            fetch(`${config.URL}?country=${country}&q=${city}&APPID=${config.APPID}&units=${units1}`)
                 .then(response => response.json())
-                .then(json => showWeather(json, event.target, config));
+                .then(json => showWeather(json, event.target, config, units1));
         })
 
     })
     .catch(error => console.log(error));
 
-function showWeather(json, target, config) {
+// как-то много параметров передается (4).
+// Лучше будет units1 - как let первой строкой js объявить?
+// все равно - как- то не очень.
+// А если для разных языков? не писать же эти параметры вручную...
+function showWeather(json, target, config, units1) {
 
     console.log(json);
     console.log(config);
@@ -32,7 +36,7 @@ function showWeather(json, target, config) {
     let html = '';
     config.weatherFieldConig.forEach((el => {
         html += '<div class="row">';
-        html += `<div class="col-6">${el.name}</div><div class="col-6">${eval('json.' + el.field)}</div>`
+        html += `<div class="col-6">${el.name}</div><div class="col-6">${eval('json.' + el.field)}, ${eval('el.units.' + units1)}</div>`
         html += '</div>';
     }))
 
